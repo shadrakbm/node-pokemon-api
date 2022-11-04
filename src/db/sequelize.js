8bceb38e9b31d798c81aa04782c71d20de762edf
Shadrak <78544510+shadrakbm@users.noreply.github.com>
2022-11-04 14:30:08 +0100

    Add files via upload

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -1,20 +1,37 @@
 const { Sequelize, DataTypes } = require('sequelize')
+const bcrypt = require('bcrypt')
 const PokemonModel = require('../models/pokemon')
-const pokemons = require('./mock-pokemons')
+const UserModel = require('../models/user')
+const pokemons = require('./mock-pokemon')
+
+let sequelize
+
+if(process.env.NODE_ENV === 'production') {
+  sequelize = new Sequelize('kk8u5y871hfoaw9y', 't09tvm6qofrtvc7h', 'ryujse9ftf40wpqn', {
+    host: 'klbcedmmqp7w17ik.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    dialect: 'mariadb',
+    dialectOptions: {
+      timezone: 'Etc/GMT-2',
+    },
+    logging: true
+  })
+} else {
+  sequelize = new Sequelize('pokedex', 'username', 'password', {
+    host: '192.168.64.2',
+    dialect: 'mariadb',
+    dialectOptions: {
+      timezone: 'Etc/GMT-2',
+    },
+    logging: true
+  })
   
-const sequelize = new Sequelize('pokedex', 'root', '', {
-  host: 'localhost',
-  dialect: 'mariadb',
-  dialectOptions: {
-    timezone: 'Etc/GMT-2',
-  },
-  logging: false
-})
-  
+}
+
 const Pokemon = PokemonModel(sequelize, DataTypes)
-  
+const User = UserModel(sequelize, DataTypes)
+
 const initDb = () => {
-  return sequelize.sync({force: true}).then(_ => {
+  return sequelize.sync().then(_ => {
     pokemons.map(pokemon => {
       Pokemon.create({
         name: pokemon.name,
@@ -22,12 +39,18 @@ const initDb = () => {
         cp: pokemon.cp,
         picture: pokemon.picture,
         types: pokemon.types
-      }).then(pokemon => console.log(pokemon.toJSON()))
+      })
+      .then(pokemon => console.log(pokemon.toJSON()))
     })
+
+    bcrypt.hash('pikachu', 10)
+    .then(hash => User.create({ username: 'pikachu', password: hash }))
+    .then(user => console.log(user.toJSON()))
+
     console.log('La base de donnée a bien été initialisée !')
   })
 }
-  
+
 module.exports = { 
-  initDb, Pokemon
+  initDb, Pokemon, User
 }
